f4c68aa5938ad78ead3f37bdaa340ed0e71224ee
yangjinhu <1191681452@qq.com>
2026-07-20 23:53:32 +0800

    fix: 修复部署报错

--- client/app/api/plans/[id]/retry/route.ts
+++ client/app/api/plans/[id]/retry/route.ts
@@ -1,7 +1,8 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
 export async function POST(
+  request: NextRequest | Request,
   { params }: { params: { id: string } }
 ) {
   try {
