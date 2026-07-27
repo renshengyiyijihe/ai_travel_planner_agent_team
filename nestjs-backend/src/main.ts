84f1723c90cffc4220cbb26c549bee5a19785ee0
yangjinhu <1191681452@qq.com>
2026-07-27 08:57:21 +0800

    feat: 忽略打包报错

--- nestjs-backend/src/main.ts
+++ nestjs-backend/src/main.ts
@@ -7,6 +7,28 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
+
+// 开启 CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',       // 本地开发前端
+      'https://ai-travel-planner-agent.vercel.app', // Vercel 前端
+    ],
+    credentials: true,
+    methods: [
+      'GET',
+      'POST',
+      'PUT',
+      'PATCH',
+      'DELETE',
+      'OPTIONS',
+    ],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+    ],
+  });
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
 
