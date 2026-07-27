263e80370656cc849610ab1d2bef433204e520d3
yangjinhu <1191681452@qq.com>
2026-07-27 15:39:44 +0800

    feat: 忽略打包报错

---
+++ nestjs-backend/src/api/index.ts
@@ -0,0 +1,38 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from '../app.module';
+import express from 'express';
+
+let server: express.Express;
+
+
+async function bootstrap() {
+
+  const expressApp = express();
+
+
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+
+  await app.init();
+
+
+  return expressApp;
+}
+
+
+export default async function handler(req, res) {
+
+  server = server ?? await bootstrap();
+
+  return server(req, res);
+}
